2d667fa7a860d72bac0f7f1f2d95a7f80f39d0db
hdmartinezduque <hd.martinezduque@gmail.com>
2025-08-10 09:13:01 -0500

    Cleaning up withdrow unneccesary class

--- src/main/java/com/example/jwt_auth_service/exception/AccessDeniedException.java
+++
@@ -1,7 +0,0 @@
-package com.example.jwt_auth_service.exception;
-
-public class AccessDeniedException extends RuntimeException {
-    public AccessDeniedException(String message){
-        super(message);
-    }
-}
